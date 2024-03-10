5e247efb68047ce2a773d14b776687d1f553e1ff
mamunozb <ma.munozb1@uniandes.edu.co>
2024-03-10 18:11:02 -0500

    pantalla de usuarios

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,11 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
+
 })
 export class UsersComponent {
 
